d5f0515ef6026e4702caec522252d717636f5c11
userbhai <127206359+userbhai@users.noreply.github.com>
2023-03-07 13:25:06 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Change one first time");
+        System.out.println("second time change");
+        System.out.println("third time change");
+        System.out.println("forth time changes");
+    }
+}
