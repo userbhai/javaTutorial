0ee5448627f868d4cd28112fd5e5fa3baeda7f9f
userbhai <127206359+userbhai@users.noreply.github.com>
2023-03-07 13:25:40 +0000

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Change one first time");
-        System.out.println("second time change");
-        System.out.println("third time change");
-        System.out.println("forth time changes");
-    }
-}
